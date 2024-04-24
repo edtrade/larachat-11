f495ae16e65bc24979f1f19856ed7c0fb5e97bbd
Ed <you@example.com>
2024-04-23 22:06:15 -0400

    wip

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user1_id',
+        'user2_id',
+        'last_message_id'
+    ];    
 }
