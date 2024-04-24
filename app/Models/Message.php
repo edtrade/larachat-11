beb9d45f2ef40532389b994a960f5bbcc1802040
Ed <you@example.com>
2024-04-23 21:56:05 -0400

    add message, sender_id, receiver_id, group_id to fillable array in Message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+        'sender_id',
+        'group_id',
+        'receiver_id',
+    ];     
 }
